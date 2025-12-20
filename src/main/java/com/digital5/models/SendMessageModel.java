fbf9a0cbf813c82e3cbfb2877f6dcd443e6bc6e6
HAIDAR NOUREDDINE <130915985+HaidarCodes@users.noreply.github.com>
2025-12-20 16:01:06 +0100

    commented and fixed java version errors

--- src/main/java/com/digital5/models/SendMessageModel.java
+++ src/main/java/com/digital5/models/SendMessageModel.java
@@ -5,6 +5,6 @@ public class SendMessageModel {
     private String jwt;
     private String message;
     private String recipient;
-    // etc...
+    // etc... in DR_interfaces ist ein mögliches modell
 
 }
