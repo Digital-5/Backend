0931ec61ee0329bf80cc25d4c88c6208d4c7fb93
HAIDAR NOUREDDINE <130915985+HaidarCodes@users.noreply.github.com>
2026-03-10 14:19:47 +0100

    c4 converter and simplified accountcontroller

--- src/main/java/com/digital5/service/StringService.java
+++ src/main/java/com/digital5/service/StringService.java
@@ -2,22 +2,19 @@ package com.digital5.service;
 
 public class StringService {
 
-    public static String cleanString(String str) {
-        if (str == null) return ""; // Handle null case
-        str = str.trim(); // Step 1: Trim whitespace
-        str = str.replaceAll("\\W+", ""); // Step 2: Remove special characters
-        str = str.replaceAll("\s+", " "); // Step 3: Replace multiple spaces with a single space
-        return str;
-    }
+    public static boolean isValidString(String str, int maxLength) {
+        if (str == null) {
+            return false;
+        } // Handle null case
+        else {
+            String str2 = str.trim(); // Step 1: Trim whitespace
+            str2 = str2.replaceAll("[^\\w\\s]", ""); // Step 2: Remove special characters
+            str2 = str2.replaceAll("\\s+", ""); // Step 3: Replace multiple spaces with a single space
 
-    public static String sizeLimitString(String str, int maxLength) {
-        if (str == null) return ""; // Handle null case
-        if (str.length() <= maxLength) {
-            return str;
-        } else {
-            return str.substring(0, maxLength); // schneidet alles nach max length ab
+            return str.length() <= maxLength && !str.isEmpty() && str.equals(str2);
         }
     }
+ 
 
     public static String stringToHex(String str) {
 
@@ -41,6 +38,33 @@ public class StringService {
             outputString.append(character);
         }
 
+        return outputString.toString();
+    }
+
+    public static String stringToC4(String str) {
+        StringBuilder c4String = new StringBuilder();
+        char[] characters = str.toCharArray();
+        for (char c : characters) {
+            int intValue = (int) c; //each char gets transformed into Unicode
+            String c4Value = Integer.toString(intValue, 36); //and then to base 36
+            // Pad to 2 characters for consistent decoding
+            if (c4Value.length() < 2) {
+                c4String.append("0");
+            }
+            c4String.append(c4Value);
+        }
+        return c4String.toString();
+    }
+
+    public static String c4ToString(String c4Str) {
+        char[] tempchar = c4Str.toCharArray();
+        StringBuilder outputString = new StringBuilder();
+
+        for (int x = 0; x < tempchar.length; x += 2) {
+            String tempString = "" + tempchar[x] + tempchar[x + 1]; //Alle 2 zeichen zusammen
+            char character = (char) Integer.parseInt(tempString, 36); //die 2 base36-Zeichen zu einem int und dann zu einem char
+            outputString.append(character);
+        }
         return outputString.toString();
     }
 }
