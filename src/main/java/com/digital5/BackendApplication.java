3f991ab65ce6268d1de45c22a3d3457d409cb498
Ewan Fabiani <ewan.fabiani@mercedes-benz.com>
2026-04-15 13:14:23 +0200

    Trying to print every env variable to see if thats the issue...

--- src/main/java/com/digital5/BackendApplication.java
+++ src/main/java/com/digital5/BackendApplication.java
@@ -2,12 +2,29 @@ package com.digital5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication app = new SpringApplication(BackendApplication.class);
+        app.addListeners((ApplicationEnvironmentPreparedEvent event) -> {
+            ConfigurableEnvironment env = event.getEnvironment();
+            env.getPropertySources()
+                    .stream()
+                    .filter(ps -> ps instanceof MapPropertySource)
+                    .map(ps -> ((MapPropertySource) ps).getSource().keySet())
+                    .flatMap(Collection::stream)
+                    .distinct()
+                    .sorted()
+                    .forEach(key -> System.out.println("key: " + key + "; property: " + env.getProperty(key)));
+        });
+        app.run(args);
     }
 
 }
