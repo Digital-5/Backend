6680ecc8ea8f0347213a32478daecd5281f01a38
Ewan Fabiani <ewan.fabiani@mercedes-benz.com>
2026-04-16 14:12:44 +0200

    removed dataresponse

--- src/main/java/com/digital5/controller/AccountController.java
+++ src/main/java/com/digital5/controller/AccountController.java
@@ -4,18 +4,13 @@ import com.digital5.exception.DigitalException;
 import com.digital5.data.models.RegisterModel;
 import com.digital5.service.AccountService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/account")
 public class AccountController {
-
-    //todo
-    // for details check .drawio file
 
     private AccountService accountService;
 
