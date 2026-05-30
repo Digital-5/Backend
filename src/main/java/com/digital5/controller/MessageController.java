9c17405d9f3d07690acd9184217aa6ac65ac83a4
Ewan Fabiani <ewan.fabiani@mercedes-benz.com>
2026-05-30 13:32:54 +0200

    Deleted useless class, refactored methods that used it

--- src/main/java/com/digital5/controller/MessageController.java
+++ src/main/java/com/digital5/controller/MessageController.java
@@ -1,6 +1,6 @@
 package com.digital5.controller;
 
-import com.digital5.data.models.JWTModel;
+import com.digital5.data.models.AuthenticationModel;
 import com.digital5.data.models.SendMessageModel;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/messages")
 public class MessageController {
 
-
     @PostMapping("/send")
     public void sendMessage(@RequestBody SendMessageModel sendMessageModel) {
     }
 
     @PostMapping("/get")
-    public void getMessages(@RequestBody JWTModel jwtModel) {
+    public void getMessages(@RequestBody AuthenticationModel jwtModel) {
     }
 }
